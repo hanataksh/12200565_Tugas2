34054ceee8c3bc5288545c27324a9c2bd5200dd3
hanataksh <hanatasalsabila@gmail.com>
2021-09-25 18:22:16 +0700

    menambahkan framework

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{ 
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Hanata Karrena Salsabila
+		     Saya tinggal di daerah Tasikmalaya,
+			 tepatnya di Jl.Lingkar Dadaha Gg.Praja No.144 A
+			 No. HP 082217494431
+			 Hobi saya adalah membaca novel dan menonton film";
+	}
+}
+?>
